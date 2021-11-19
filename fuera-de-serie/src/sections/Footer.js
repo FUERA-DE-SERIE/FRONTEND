3436f808d252b087bbfb30fb6905a6e348cffd3a
Daniel Ayala <Hogar-PC@Hogar.(none)>
2021-11-18 22:02:20 -0500

    creacion del footer

--- fuera-de-serie/src/sections/Footer.js
+++ fuera-de-serie/src/sections/Footer.js
@@ -1,14 +1,49 @@
 import Button1 from "../components/buttons/Button1"
+import './Footer.css'
 
 const Footer = () => {
     return (
-        <div>
-            <form action=''>
-                <input type='email' placeholder='Ingresa tu correo electronico' />
-                <Button1 buttonName='Suscribirse'/>
-            </form>
-        </div>
+        <>
+            <footer class="section-footer">
+                <section class="container">
+                    <div class="container-Subscribe">
+                        <h1>Suscribete a Nuestra Newsletter</h1>
+                    </div>
+                    <input 
+                        type="email" 
+                        placeholder="Ingresa tu correo electronico"
+                        class="container-Email"> 
+                    </input>
+                    <Button1 buttonName='Suscribirse'/>     
+                </section>
+                <div class="container-dos">
+                    <div class="LinksRapidos">
+                        <h2>LINKS RAPIDOS</h2>
+                        <h3>Fuera de Serie 2021</h3>
+                        <h3>Fuera de Serie 2021</h3>
+                    </div>
+                    <div class="LinksRapidos-dos">
+                        <h2>LINKS RAPIDOS</h2>
+                        <h3>Fuera de Serie 2021</h3>
+                        <h3>Fuera de Serie 2021</h3>
+                    </div>            
+                    <div class="Title">
+                        <h4>MARTINY</h4>
+                    </div>
+                    <div>
+                        <hr/>
+                    </div>
+                    <div class="final">
+                        <h3>Link repositorio en GitHub Fuera de Serie</h3>
+                    </div>
+                    <div class="final-dos">
+                        <h3>Fuera de Serie 2021</h3>
+                    </div> 
+                </div>
+            </footer>            
+        </>
     )
 }
 
 export default Footer
+
