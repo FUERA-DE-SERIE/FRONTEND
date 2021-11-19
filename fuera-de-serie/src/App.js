42b92f3f9590823e295704ef2c9dc35542798ac7
LUIS MEDINA <maira.amorutp@gmail.com>
2021-11-19 16:30:26 -0500

    Archivos cuadro 1

--- fuera-de-serie/src/App.js
+++ fuera-de-serie/src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import Footer from './sections/Footer';
+import Cuadro1 from './components/may/cuadro1';
 
 function App() {
   return (
     <div className="App">
+
+      <Cuadro1/>
+      
       <Footer/>
+  
     </div>
   );
 }
---
+++ fuera-de-serie/src/components/may/cuadro1.js
@@ -0,0 +1,29 @@
+import './cuadro1.css';
+
+
+const Cuadro1=(props)=>{
+  return(
+    <div className="user-card">
+      < div className="user-nombre">
+      <h1 class="fontPrata">NOMBRE</h1>
+        <div className="log-in">
+          <h1>Log in</h1>
+          <h2 class="fontCalibri">No tienes una cuenta?  <a class="font-negra" href="" target="" rel="">Crea una cuenta</a></h2>          
+              <br />
+              <input type="email" id="email" name="email"/>
+              <br />              
+              <br />
+              <input type="email" id="email" name="email"/>
+              <br />
+              <br />            
+            <p><button type="submit" className="button1" color="#888469" >Suscribirse</button></p>
+
+          <h2 class="fontCalibri">Olvidaste tu contraseña? <a class="font-negra-color" href="" target="" rel="">.    Recuperar </a></h2>          
+        </div>
+      </div>
+      
+    </div>
+  )
+}
+
+export default Cuadro1;
