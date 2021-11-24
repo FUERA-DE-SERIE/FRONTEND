2cfb59da5f5c7ac17acef03abbbe21442d8dcb88
DidierLLanten <didiervelez@hotmail.es>
2021-11-23 21:56:57 -0500

    agregando componente de imagen

--- fuera-de-serie/src/App.js
+++ fuera-de-serie/src/App.js
@@ -2,14 +2,17 @@
 import Footer from './sections/Footer';
 import CardLeft from './components/Cards/CardLeft';
 import CardRight from './components/Cards/CardRight';
+import CardImage from './components/Cards/CardImage';
+
+
 
 function App() {
   return (
     <div className="App">
-      <CardLeft/>
-      <CardRight/> 
-      
-      <Footer/>      
+      <CardLeft />
+      <CardRight />
+      <CardImage img={"https://placeimg.com/300/300/animals"} />
+      <Footer />
     </div>
   );
 }
