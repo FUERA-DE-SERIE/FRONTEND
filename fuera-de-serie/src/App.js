fe8ae309e808b0176f7b004dd1b0f8c69d443a78
DidierLLanten <didiervelez@hotmail.es>
2021-11-23 22:27:08 -0500

    probando componente imagen

--- fuera-de-serie/src/App.js
+++ fuera-de-serie/src/App.js
@@ -1,12 +1,15 @@
 // import './App.css';
+import CardImage from './components/Cards/CardImage';
 import Home from './components/pages/home/Home';
 import Footer from './sections/Footer';
 
+CardImage
 function App() {
   return (
     <div className="App">
+      <CardImage/>
       <Home/>
-      <Footer/>      
+      <Footer/>       
     </div>
   );
 }
