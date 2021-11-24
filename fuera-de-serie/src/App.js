cbb7389340c17ed182f9fd59c3d3f83b2f1ea3e0
DidierLLanten <didiervelez@hotmail.es>
2021-11-23 22:00:42 -0500

    actulizando rama aun

--- fuera-de-serie/src/App.js
+++ fuera-de-serie/src/App.js
@@ -9,16 +9,9 @@ import CardImage from './components/Cards/CardImage';
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      <CardLeft />
-      <CardRight />
-      <CardImage img={"https://placeimg.com/300/300/animals"} />
-      <Footer />
-=======
       <CardLeft/>
       <CardRight/> 
       <Footer/>      
->>>>>>> 70a71b222eeebce45f123ca61eddeca43e19e76d
     </div>
   );
 }
